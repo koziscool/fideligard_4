159008a7bd9ff6f42e76cbb8552ba659596e6460
koziscool <kozjob@yahoo.com>
2016-07-11 15:55:38 -0500

    first cut panel view

---
+++ app/app.js
@@ -0,0 +1,17 @@
+
+fido = angular.module('fideligard', ['ui.router'])
+console.log(fido);
+
+fido.config( function($stateProvider, $urlRouterProvider )  {
+
+  $urlRouterProvider.otherwise('/stocks');
+
+  $stateProvider
+
+  .state( 'stocks', {
+    url: '/stocks',
+    controller: 'StocksCtrl',
+    templateUrl: '/app/templates/stocks.html',
+  });
+
+});
---
+++ app/controllers/stocks_controller.js
@@ -0,0 +1,6 @@
+fido.controller('StocksCtrl', ['$scope', '$stateParams', 'PortfolioService', function($scope, $stateParams, PortfolioService) {
+
+  $scope.stocks = PortfolioService.getStocks();
+  console.log($scope.stocks);
+
+}]);
